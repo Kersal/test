bafb5c432c2dc683ea98390ae777ef42816c8e78
Evgenios George Moiris <evgeniosmoiris@gmail.com>
2021-12-28 19:56:40 +0200

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Ευγένιος Γεώργιος Μοίρης on 28/12/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
